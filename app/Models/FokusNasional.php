7bd497fc7aa7e27048bcaacc7ce78829620b344c
faisal-gif <fagdera@gmail.com>
2026-05-19 10:57:08 +0700

    feat: Update FokusNasional model to use fillable properties

--- app/Models/FokusNasional.php
+++ app/Models/FokusNasional.php
@@ -9,7 +9,15 @@ class FokusNasional extends Model
     protected $connection = 'mysql_nasional';
     protected $table = 'news_focus';
     protected $primaryKey = 'focnews_id';
-    protected $guarded = [];
+    protected $fillable = [
+        'focnews_title',
+        'focnews_description',
+        'focnews_keyword',
+        'focnews_image_body',
+        'focnews_image_news',
+        'focnews_image_mobile',
+        'created_by',
+    ];
 
     const CREATED_AT = 'created';
     const UPDATED_AT = 'modified';
