59adab6e715ca89054fc3b2f0199dbfac8987722
faisal-gif <fagdera@gmail.com>
2026-06-12 23:34:51 +0700

    feat: add SSO redirect middleware to application routing

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\SsoRedirectToNewApp;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -17,6 +18,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->web(append: [
             \App\Http\Middleware\HandleInertiaRequests::class,
             \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
+            SsoRedirectToNewApp::class,
         ]);
         // Mendaftarkan Alias Middleware untuk Spatie Permission
         $middleware->alias([
