7a7e5227d406439d8046509ce54b52e34a372039
faisal-gif <fagdera@gmail.com>
2026-07-17 19:28:58 +0700

    refactor: change TopCategoryNasionalExport to implement FromCollection and improve query handling

--- app/Exports/TopCategoryNasionalExport.php
+++ app/Exports/TopCategoryNasionalExport.php
@@ -5,14 +5,14 @@ namespace App\Exports;
 use App\Models\NewsNasional;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\DB;
-use Maatwebsite\Excel\Concerns\FromQuery;
+use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\Exportable;
 use Maatwebsite\Excel\Concerns\WithHeadings;
 use Maatwebsite\Excel\Concerns\WithMapping;
 use Maatwebsite\Excel\Concerns\WithStyles;
 use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
 
-class TopCategoryNasionalExport implements FromQuery, WithHeadings, WithMapping, WithStyles
+class TopCategoryNasionalExport implements FromCollection, WithHeadings, WithMapping, WithStyles
 {
     use Exportable;
 
@@ -24,7 +24,7 @@ class TopCategoryNasionalExport implements FromQuery, WithHeadings, WithMapping,
         $this->filters = $filters;
     }
 
-    public function query()
+    public function collection()
     {
         $query = NewsNasional::query()
             ->join('news_category', 'news.catnews_id', '=', 'news_category.catnews_id')
@@ -47,9 +47,8 @@ class TopCategoryNasionalExport implements FromQuery, WithHeadings, WithMapping,
         }
 
         if (!empty($this->filters['tag'])) {
-            $query->whereHas('tags', function ($q) {
-                $q->where('tags.id', $this->filters['tag']);
-            });
+            $tagId = $this->filters['tag'];
+            $query->whereHas('tags', fn($q) => $q->where('tags.id', $tagId));
         }
 
         return $query
@@ -59,9 +58,9 @@ class TopCategoryNasionalExport implements FromQuery, WithHeadings, WithMapping,
                 DB::raw('SUM(COALESCE(news_views.pageviews, 0)) as total_views')
             )
             ->groupBy('news_category.catnews_id', 'news_category.catnews_title')
-            ->orderBy('total_views', 'DESC');
+            ->orderByRaw('SUM(COALESCE(news_views.pageviews, 0)) DESC')
+            ->get();
     }
-    
 
     public function headings(): array
     {
@@ -77,16 +76,12 @@ class TopCategoryNasionalExport implements FromQuery, WithHeadings, WithMapping,
             $row->catnews_title,
             (int) $row->total_berita,
             (int) $row->total_views,
-            $row->total_berita > 0
-                ? round($row->total_views / $row->total_berita, 1)
-                : 0,
+            $row->total_berita > 0 ? round($row->total_views / $row->total_berita, 1) : 0,
         ];
     }
 
     public function styles(Worksheet $sheet)
     {
-        return [
-            1 => ['font' => ['bold' => true]],
-        ];
+        return [1 => ['font' => ['bold' => true]]];
     }
 }
