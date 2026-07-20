5d7a0cd73526a6ac6e2fc0960ef11f221ef54d4c
faisal-gif <fagdera@gmail.com>
2026-07-20 12:35:20 +0700

    feat: add PengumumanWebBerbayar model for managing paid announcements

---
+++ app/Models/PengumumanWebBerbayar.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PengumumanWebBerbayar extends Model
+{
+
+    use HasFactory;
+
+    protected $connection = 'mysql_berbayar';
+    protected $table = 'pengumuman';
+    protected $fillable = [
+        'title',
+        'content',
+        'type',
+        'start_date',
+        'end_date',
+        'is_active',
+        'created_by'
+    ];
+
+    protected $casts = [
+        'start_date' => 'datetime',
+        'end_date' => 'datetime',
+    ];
+}
